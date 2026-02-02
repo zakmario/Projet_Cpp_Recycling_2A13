4a3581536bae6de9d28badaeb67247f3ebd6ab86
Zakaria Chahbani <Zakaria.Chahbani@esprit.tn>
2026-02-02 22:19:25 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Projet C++ Recycling - Branche gestion-zones" << endl;
+    cout << "Développeur : Zakaria Chahbani" << endl;
+    return 0;
+}
